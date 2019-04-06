7b08126f560bd6c0444d84026ecae3e65b2c8712
Lei Huang <lh389@cornell.edu>
2019-04-06 15:22:33 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='ttea',
+      version='0.1',
+      description='Time to event analysis',
+      author='Lei Huang',
+      author_email='lh389@cornell.edu',
+      url='https://github.com/leihuang/ttea',
+      packages=['distutils', 'distutils.command'],
+     )
